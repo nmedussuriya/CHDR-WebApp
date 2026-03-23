a840a74329c4f6418d6107652c8789df4a48d646
Manuja098 <manujasiriwardhana126@gmail.com>
2026-03-23 14:37:07 +0530

    Update firebaseconfig.js

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -1,1 +1,19 @@
+ import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD0ZNSZzneSSwrC2yoSekL6vQ6wZzeFijM",
+  authDomain: "childhealthcaredb.firebaseapp.com",
+  projectId: "childhealthcaredb",
+  storageBucket: "childhealthcaredb.firebasestorage.app",
+  messagingSenderId: "919347967013",
+  appId: "1:919347967013:web:c91a376e2bc8ab2c7e7bcc"
+};
+
+const app = initializeApp(firebaseConfig);
+
+ 
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export default app;
